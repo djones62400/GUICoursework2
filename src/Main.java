5ed5614095d98736fde3703254fbd14a58d4580a
djones62400 <djones.62400@farnborough.ac.uk>
2017-11-24 14:32:47 +0000

    2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
 
 public class Main extends Application {
 
@@ -13,6 +15,7 @@ public class Main extends Application {
 
         Pane root = new Pane();
 
+        root.getStylesheets().add("stylesheet.css");
 
         Scene scene = new Scene(root, 1280, 900);
         scene.getStylesheets().add("stylesheet.css");
@@ -21,19 +24,30 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.setResizable(false);
 
+        Text pageTitle = new Text();
+        pageTitle.setText("or");
+        root.getChildren().add(pageTitle);
+        pageTitle.setLayoutX(640);
+        pageTitle.setLayoutY(450);
+        Font.font(36);
+
         Button checkIn = new Button("Check in");
-        checkIn.getStyleClass().add("button");
-        checkIn.setLayoutX(550);
-        checkIn.setLayoutY(100);
+        checkIn.getStyleClass().add("button1");
+        checkIn.setLayoutX(390);
+        checkIn.setLayoutY(200);
+        checkIn.setPrefWidth(500);
+        checkIn.setPrefHeight(200);
         root.getChildren().add(checkIn);
 
         Button checkOut = new Button("Check Out");
-        checkOut.getStyleClass().add("button");
-        checkOut.setLayoutX(550);
+        checkOut.getStyleClass().add("button2");
+        checkOut.setLayoutX(390);
         checkOut.setLayoutY(500);
+        checkOut.setPrefWidth(500);
+        checkOut.setPrefHeight(200);
         root.getChildren().add(checkOut);
 
-        checkIn.setOnAction((ActionEvent ae) -> doSomething(ae));
+        checkOut.setOnAction((ActionEvent ae) -> doSomething(ae));
         //boolean add = root.getChildren().add(checkIn);
         stage.show();
 
